a13ce28454efea7cc1b6060efd8bb8cb6173ffae
Axel <axelvalentprayogo@gmail.com>
2026-05-15 10:31:08 +0700

    feat: Add HomeScene animations and home-to-game navigation
      - Logo subtle wobble (tilt left/right, easeInEaseOut)
      - Play button idle pulse, press shrink, drag-cancel behavior
      - Wire HomeView and ContentView to navigate Home → Game

--- PaoApp/UI/Screens/HomeView.swift
+++ PaoApp/UI/Screens/HomeView.swift
@@ -5,14 +5,29 @@
 //  Created by Saujana Shafi on 10/05/26.
 //
 
+import SpriteKit
 import SwiftUI
 
 struct HomeView: View {
+    var onPlay: () -> Void
+
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            SpriteView(scene: makeScene(size: proxy.size))
+                .ignoresSafeArea()
+                .frame(width: proxy.size.width, height: proxy.size.height)
+        }
+        .ignoresSafeArea()
+    }
+
+    private func makeScene(size: CGSize) -> HomeScene {
+        let scene = HomeScene(fileNamed: "HomeScene") ?? HomeScene(size: size)
+        scene.scaleMode = .aspectFill
+        scene.onPlayTapped = onPlay
+        return scene
     }
 }
 
 #Preview {
-    HomeView()
+    HomeView(onPlay: {})
 }
