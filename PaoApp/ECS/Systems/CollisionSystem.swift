e9556cb56475197c044c12eac35fdd2908c0deed
Saujana Shafi <hello@saujanashafi.com>
2026-05-18 09:25:52 +0700

    fix: Revise contact system

--- PaoApp/ECS/Systems/CollisionSystem.swift
+++ PaoApp/ECS/Systems/CollisionSystem.swift
@@ -23,19 +23,28 @@ class CollisionSystem: GKComponentSystem<PhysicsComponent> {
 
         for physicsComponent in components {
             for contact in physicsComponent.contactQueue {
-                if let healthComponent = contact.entityA.component(
+                guard
+                    let nodeA = contact.bodyA.node,
+                    let entityA = entityManager.entity(forNode: nodeA),
+                    let nodeB = contact.bodyB.node,
+                    let entityB = entityManager.entity(forNode: nodeB)
+                else {
+                    continue
+                }
+
+                if let healthComponent = entityA.component(
                     ofType: HealthComponent.self
                 ) {
                     healthComponent.hit()
-                } else if let healthComponent = contact.entityB.component(
+                } else if let healthComponent = entityB.component(
                     ofType: HealthComponent.self
                 ) {
                     healthComponent.hit()
-                } else if let consumableComponent = contact.entityA.component(
+                } else if let consumableComponent = entityA.component(
                     ofType: ConsumableComponent.self
                 ) {
                     // TODO: Handle player consuming item
-                } else if let consumableComponent = contact.entityB.component(
+                } else if let consumableComponent = entityB.component(
                     ofType: ConsumableComponent.self
                 ) {
                     // TODO: Handle player consuming item
