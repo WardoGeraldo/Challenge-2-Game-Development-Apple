02d127d6db4456f08c2a94ce11cc42f20ce8c111
Saujana Shafi <hello@saujanashafi.com>
2026-05-18 09:32:32 +0700

    feat: Controller system

--- PaoApp/ECS/Components/ControlComponent.swift
+++ PaoApp/ECS/Components/ControlComponent.swift
@@ -9,5 +9,26 @@ import Foundation
 import GameplayKit
 
 class ControlComponent: GKComponent {
+    var pointTo: CGPoint
 
+    var constraint: SKConstraint {
+        return SKConstraint.orient(
+            to: self.pointTo,
+            offset: SKRange(constantValue: -CGFloat.pi / 2),
+        )
+    }
+
+    func orient(to position: CGPoint) {
+        self.pointTo = position
+    }
+
+    init(pointTo: CGPoint) {
+        self.pointTo = pointTo
+
+        super.init()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
