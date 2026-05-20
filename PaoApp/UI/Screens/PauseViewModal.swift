5cef0101d7bf3588a1323c144eda046a0437823a
WardoGeraldo <egeraldo@student.ciputra.ac.id>
2026-05-20 12:42:39 +0700

    Pause setting and quit confirm view, 95% done. Added new quit modal asset

--- PaoApp/UI/Screens/PauseViewModal.swift
+++ PaoApp/UI/Screens/PauseViewModal.swift
@@ -5,4 +5,40 @@
 //  Created by Axel Valent Prayogo on 20/05/26.
 //
 
-import Foundation
+import SwiftUI
+import SpriteKit
+
+struct PauseViewModal: View {
+    var onResume: () -> Void
+    var onQuit: () -> Void
+    
+    var body: some View {
+        GeometryReader { proxy in
+            SpriteView(scene: makeScene(size: proxy.size), options: [.allowsTransparency])
+                .ignoresSafeArea()
+                .frame(width: proxy.size.width, height: proxy.size.height)
+        }
+        .ignoresSafeArea()
+    }
+    
+    private func makeScene(size: CGSize) -> PauseSettingScene {
+        // Loads the .sks file 
+        let scene = PauseSettingScene(fileNamed: "PauseSettingScene") ?? PauseSettingScene(size: size)
+        
+        scene.scaleMode = .aspectFill
+        scene.onResume = onResume
+        scene.onQuit = onQuit
+        
+        // .clear background
+        scene.backgroundColor = .clear
+        
+        return scene
+    }
+}
+
+#Preview {
+    ZStack {
+//        Color.blue  //Placeholder game background
+        PauseViewModal(onResume: {}, onQuit: {})
+    }
+}
